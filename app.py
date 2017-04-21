344594c083b71f10551ebcb2bee95ce4cc4a6f16
M-Yusuf <m.yusuf0377@gmail.com>
2017-04-20 22:38:18 -0400

    Initial

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+# Here's how you create a route
+# @app.route("/routeName")
+# def functionName():
+#    return render_template("fileName.html")
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=4000)
