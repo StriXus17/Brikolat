6e7dd8b99e74e41a51365a7e5a627537bbcc2570
StriX Tech <StriXus17>
2023-06-09 21:57:44 +0200

    Signed-off-by: StriX Tech <StriXus17>

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
+import os
+import json
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    with open('./db/data.json') as f:
+        data = json.load(f)
+    return render_template("home.html",data=data,l=len(data))
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/new",methods=["GET","POST"])
+def new():
+    if request.method == "POST":
+        with open('./db/data.json',"r") as f:
+            data = json.load(f)
+            d = {
+            "name":request.form["name"] +" "+ request.form["familyName"],
+            "job":request.form["jobType"],
+            "baladia":request.form["town"],
+            "wilaya":request.form["state"],
+            "rate":0,
+            "tel":request.form["phoneNumber"],
+            "location":None}
+            data.append(d)
+            f.close()
+        with open('./db/data.json',"w") as f:
+            json.dump(data, f)
+            f.close()
+            return redirect("/")
+    return render_template("create-account.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 
 # Here's how you create a route
 # @app.route("/routeName")
